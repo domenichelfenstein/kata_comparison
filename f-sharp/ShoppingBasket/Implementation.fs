a52b9aa7468945cc60da26ec5205a8ae60866c34
Domenic Helfenstein <d.helfenstein@calitime.ch>
2022-09-05 15:58:24 +0200

    f# implementation of shopping cart kata

---
+++ f-sharp/ShoppingBasket/Implementation.fs
@@ -0,0 +1,25 @@
+﻿module FSharp.ShoppingBasket
+
+type Product = { Id : string ; Name : string ; Price : decimal }
+    
+module Basket =
+    let totalCost products basket =
+        let discounts =
+            let fivePercent total = if total > 100m then Some (total * 0.05m) else None
+            let tenPercent total = if total > 200m then Some (total * 0.10m) else None
+            [ fivePercent; tenPercent ]
+        
+        let applyDiscount total =
+            discounts
+            |> List.tryFindBack (fun f -> f total |> Option.isSome)
+            |> Option.bind (fun f -> f total)
+            |> Option.map (fun discount -> total - discount)
+            |> Option.defaultValue total
+        
+        let roundToTwoDecimals (total: decimal) = System.Math.Round(total, 2)
+        
+        basket
+        |> List.map (fun item -> products |> List.find (fun p -> p.Id = item))
+        |> List.sumBy (fun product -> product.Price)
+        |> applyDiscount
+        |> roundToTwoDecimals
---
+++ f-sharp/ShoppingBasket/Tests.fs
@@ -0,0 +1,96 @@
+﻿module ``Shopping Basket``
+
+open Xunit
+open FsUnit.Xunit
+open FSharp.ShoppingBasket
+open Swensen.Unquote
+
+[<Fact>]
+let ``returns 0 if basket is empty`` () =
+    let basket = []
+    let total = basket |> Basket.totalCost []
+    total |> should equal 0m
+
+[<Fact>]
+let ``returns cost of product if basket contains single item`` () =
+    let basket = [ "001" ]
+
+    let product =
+        { Id = "001"
+          Name = "Apple"
+          Price = 2.50m }
+
+    let total =
+        basket |> Basket.totalCost [ product ]
+
+    total |> should equal 2.50m
+
+[<Fact>]
+let ``returns total if basket contains product twice`` () =
+    let basket = [ "001"; "001" ]
+
+    let product =
+        { Id = "001"
+          Name = "Apple"
+          Price = 2.50m }
+
+    let total =
+        basket |> Basket.totalCost [ product ]
+
+    total |> should equal 5m
+
+[<Fact>]
+let ``returns total if basket contains different products`` () =
+    let basket = [ "001"; "002" ]
+
+    let apple =
+        { Id = "001"
+          Name = "Apple"
+          Price = 2.50m }
+
+    let pear =
+        { Id = "002"
+          Name = "Pear"
+          Price = 2.90m }
+
+    let total =
+        basket |> Basket.totalCost [ apple; pear ]
+
+    total |> should equal 5.40m
+
+[<Fact>]
+let ``returns discounted total if total > $100`` () =
+    let basket =
+        [ "001"
+          "001"
+          "001"
+          "001"
+          "001"
+          "002"
+          "002"
+          "003"
+          "003"
+          "003"
+          "003"
+          "003"
+          "003" ]
+
+    let a =
+        { Id = "001"
+          Name = "A"
+          Price = 10m }
+
+    let b =
+        { Id = "002"
+          Name = "B"
+          Price = 25m }
+
+    let c =
+        { Id = "003"
+          Name = "C"
+          Price = 9.99m }
+
+    let total =
+        basket |> Basket.totalCost [ a; b ; c ]
+
+    total |> should equal 151.94m
