4bcaa1dddbade46a12299b54d36f6822db6c01de
Domenic Helfenstein <d.helfenstein@calitime.ch>
2022-09-07 07:51:36 +0200

    F# StringCalculator (parser style)

---
+++ f-sharp/StringCalculator/Implementation.fs
@@ -0,0 +1,46 @@
+﻿module FSharp.StringCalculator
+
+type ParsePart =
+    | Digit of int
+    | Operator of Operation
+
+and Expression =
+    | Number of int
+    | Operation of Operation * Expression * Expression
+    | Inconclusive of int * Operation
+    | Empty
+
+and Operation =
+    | Plus
+    | Minus
+
+let parseChar c =
+    match c with
+     | s when s >= '0' && s <= '9' -> Digit (int (string c))
+     | '+' -> Operator Plus
+     | '-' -> Operator Minus
+        | _ -> failwith "Invalid character"
+     
+let rec evaluate expression =
+    match expression with
+    | Number n -> n
+    | Operation (Plus, left, right) -> evaluate left + evaluate right
+    | Operation (Minus, left, right) -> evaluate left - evaluate right
+    | Empty -> 0
+    | Inconclusive _ -> failwith "Inconclusive expression"
+    
+let rec toTree (expression: Expression) currentPart = 
+    match expression, currentPart with
+     | Empty, Digit d -> Number d
+     | Empty, Operator o -> Inconclusive (0, o)
+     | Number n, Digit d -> Number (n * 10 + d)
+     | Number n, Operator o -> Inconclusive (n, o)
+     | Inconclusive (n, o), Digit d -> Operation (o, Number n, Number d)
+     | Operation (o, left, right), Digit d -> Operation (o, left, (toTree right (Digit d)))
+     | _ -> failwith "Invalid expression"
+
+let calculate (expression: string) =
+    expression
+        |> Seq.map parseChar
+        |> Seq.fold toTree Empty
+        |> evaluate
---
+++ f-sharp/StringCalculator/Tests.fs
@@ -0,0 +1,26 @@
+﻿module ``String Calculator``
+open Xunit
+open FsUnit.Xunit
+open FSharp.StringCalculator
+open Swensen.Unquote
+
+[<Fact>]
+let ``empty string returns zero`` () =
+    let result = calculate ""
+    result |> should equal 0
+
+[<Theory>]
+[<InlineData("1", 1)>]
+[<InlineData("456", 456)>]
+[<InlineData("-2", -2)>]
+let ``single number returns the value`` (input, expected) =
+    let result = calculate input
+    result |> should equal expected
+
+[<Theory>]
+[<InlineData("1+1", 2)>]
+[<InlineData("57+100", 157)>]
+[<InlineData("1000+0", 1000)>]
+let ``addition of two numbers returns sum`` (input, expected) =
+    let result = calculate input
+    result |> should equal expected
